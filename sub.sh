c05d7a26ffc904fa2ff775b5a12a4d81cfef60e9
luo83651896 <31127597+luo83651896@users.noreply.github.com>
2020-02-24 05:30:29 +0800

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -25,8 +25,7 @@ do
     number=$?                       #获取进程数量
     if [ $number -eq 0 ]            #如果进程数量为0
     then                            #重新启动服务器，或者扩展其它内容。
-        cd config
-        ./subconverter &
+        config/subconverter &
     fi
     proc_name="python3"        #进程名
     proc_num()                      #查询进程数量
