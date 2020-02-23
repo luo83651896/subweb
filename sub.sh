9f79e6b1de7c8b42afd71458033200b279986f4e
luo83651896 <31127597+luo83651896@users.noreply.github.com>
2020-02-24 05:15:46 +0800

    Create sub.sh

---
+++ sub.sh
@@ -0,0 +1,47 @@
+#! /bin/sh 
+echo "-----------------------------------Start-------------------------------------"
+echo "-----------------------------------$1,$2------------------------"
+filename="api/aff.py"
+cat>"${filename}"<<EOF
+aff = '不限制机场，规则生成失败，请检测调用格式。STC测试可用，注册地址：bilibili.stchk.cloud/auth/register?code=gzI5'   
+apiip = '$1'
+subip = '$2'   
+passwd = 'admin'            
+EOF
+cat api/aff.py
+chmod 777 config/subconverter
+echo "-----------------------------------Update file----------------------------------------"
+echo "-------------------------------------While--------------------------------------------"
+
+while true
+do
+    proc_name="subconverter"        #进程名
+    proc_num()                      #查询进程数量
+    {
+        num=`ps -ef | grep $proc_name | grep -v grep | wc -l`
+        return $num
+    }
+    proc_num  
+    number=$?                       #获取进程数量
+    if [ $number -eq 0 ]            #如果进程数量为0
+    then                            #重新启动服务器，或者扩展其它内容。
+        cd config
+        ./subconverter &
+    fi
+    proc_name="python3"        #进程名
+    proc_num()                      #查询进程数量
+    {
+        num=`ps -ef | grep $proc_name | grep -v grep | wc -l`
+        return $num
+    }
+    proc_num  
+    number=$?                       #获取进程数量
+    if [ $number -eq 0 ]            #如果进程数量为0
+    then                            #重新启动服务器，或者扩展其它内容。
+        python3 api.py &   #运行web服务
+    fi
+
+    sleep 60s
+echo "------------------------------------Sucess----------------------------------------"
+done
+echo "------------------------------------WEB----------------------------------------"
